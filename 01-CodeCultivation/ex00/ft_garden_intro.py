604b09f965d82eb8113bdb79b9c6fcc7c1baab5c
MEDBOUAYOUNE <mbouayou@student.1337.ma>
2025-12-30 21:15:13 +0100

    ex00 | day01

---
+++ 01-CodeCultivation/ex00/ft_garden_intro.py
@@ -0,0 +1,18 @@
+def ft_garden_intro()->None:
+    """
+    This function introduces the garden plant
+    """
+    plant_name = "Rose"
+    plant_height = 25
+    plant_age = 30
+
+    print("=== Welcome to My Garden ===")
+    print("Plant: {plant_name}")
+    print("Height: {plant_height}cm")
+    print("Age: {plant_age} days")
+    print()
+    print("=== End of Program ===")
+
+
+if __name__ == "__main__":
+    ft_garden_intro()
