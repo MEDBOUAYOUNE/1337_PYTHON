d52b5da8153e240b3f26d3b068681f10bfcb221a
MEDBOUAYOUNE <mbouayou@student.1337.ma>
2025-12-30 03:22:28 +0100

    first ex00

---
+++ Growing_Code/ex00/ft_hello_garden.py
@@ -0,0 +1,18 @@
+def ft_hello_garden():
+    """
+    This function prints a greeting message.
+    """
+    print("Hello, Garden Community!")
+
+
+# Uncomment the following lines to test the function
+if __name__ == "__main__":
+    ft_hello_garden()
+
+# For running the script:
+
+# python3 ft_hello_garden.py
+
+# python3
+#  >>> from ft_hello_garden import ft_hello_garden
+#  >>> ft_hello_garden()
