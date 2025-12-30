8db733032f0419786f7056b7e3650bdf6c50fabe
MEDBOUAYOUNE <mbouayou@student.1337.ma>
2025-12-30 20:18:03 +0100

    correcting the ex04 _doc_

--- Growing_Code/ex04/ft_water_reminder.py
+++ Growing_Code/ex04/ft_water_reminder.py
@@ -1,6 +1,6 @@
 def ft_water_reminder():
     """
-    This function reminds the user to water their plants.
+    This function reminds since when the plants were last watered.
     """
     days_since_watered = int(input("Enter number of days since last watering: "))
     print("Water the plants!" if days_since_watered > 2 else "Plants are fine.")
