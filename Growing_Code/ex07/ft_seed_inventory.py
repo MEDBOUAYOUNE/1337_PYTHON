3e9c6a533b07955091304508b0962d28493f7541
MEDBOUAYOUNE <mbouayou@student.1337.ma>
2025-12-30 20:21:46 +0100

    Normalize the ex07 to match subject reqs

--- Growing_Code/ex07/ft_seed_inventory.py
+++ Growing_Code/ex07/ft_seed_inventory.py
@@ -2,11 +2,11 @@ def ft_seed_inventory(seed_type : str, seed_quantity : int, seed_unit : str):
     """
     This function displays the seed inventory.
     """
-    if seed_unit == "packets":
+    if seed_unit.lower() == "packets":
         print(f"{seed_type} seeds: {seed_quantity} {seed_unit} available")
-    elif seed_unit == "grams":
+    elif seed_unit.lower() == "grams":
         print(f"{seed_type} seeds: {seed_quantity} {seed_unit} total")
-    elif seed_unit == "area":
+    elif seed_unit.lower() == "area":
         print(f"{seed_type} seeds: {seed_quantity} square meters")
     else:
-        print(f"{seed_type} seeds: {seed_quantity} {seed_unit}")
+        print(f"Unknown unit type")
