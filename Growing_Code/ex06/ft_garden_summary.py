e8be89137696a5d0376e423fd0126ec21721f03f
MEDBOUAYOUNE <mbouayou@student.1337.ma>
2025-12-30 20:04:26 +0100

    ex06

---
+++ Growing_Code/ex06/ft_garden_summary.py
@@ -0,0 +1,9 @@
+def ft_garden_summary():
+    """
+    This function summarizes the garden's status.
+    """
+    garden_name = input("Enter garden name: ")
+    plants_number = input("Enter number of plants: ")
+    print(f"Garden: {garden_name}")
+    print(f"Plants: {plants_number}")
+    print("Status: Growing well!")
